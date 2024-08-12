86c098eb27ed3f40aa24d4e27f3c128398c030f0
Vi Le <lecongvi18052002@gmail.com>
2024-08-12 16:23:40 +0700

    check apikey and permission

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const { findById } = require("../services/apikey.service");
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization',
+}
+
+const apiKey = async(req, res, next) => {
+    try {
+        // if (req.headers[HEADER.API_KEY]) {
+        //     const key = req.headers[HEADER.API_KEY].toString();
+        // }
+
+        const key = req.headers[HEADER.API_KEY] ? req.headers[HEADER.API_KEY].toString() : null;
+
+
+        if (!key) {
+            return res.status(403).json({
+                message: 'Forbidden Error: API key is required'
+            });
+        }
+
+        // check object key
+        const objKey = await findById(key);
+
+        if (!objKey) {
+            return res.status(403).json({
+                message: 'Forbidden Error: Object key is required'
+            });
+        }
+
+        req.objKey = objKey;
+        return next();
+
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
+
+const permission = (permission) => {
+    return (req, res, next) => {
+        if (!req.objKey.permissions) {
+            return res.status(403).json({
+                message: 'Forbidden Error: Permissions required'
+            });
+        }
+
+        // check if permission is valid
+        console.log('permission::', req.objKey.permissions);
+
+        const validPermission = req.objKey.permissions.includes(permission);
+
+        if (!validPermission) {
+            return res.status(403).json({
+                message: 'Forbidden Error: Permissions denied'
+            });
+        }
+
+        return next();
+    }
+}
+
+module.exports = {
+    apiKey,
+    permission,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const express = require('express');
+const { apiKey, permission } = require('../auth/checkAuth');
 const router = express.Router();
 
+// address middlware pre handle system
+
+// check api key
+router.use(apiKey);
+
+// check permissions
+router.use(permission('0000'));
+
+// status access into account
 router.use('/v1/api', require('./access'));
 
 
