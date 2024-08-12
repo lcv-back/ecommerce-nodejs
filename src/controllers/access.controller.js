bae69e13fc207ea1081a85e32c3af74376d75ef9
Vi Le <lecongvi18052002@gmail.com>
2024-08-12 20:42:58 +0700

    completed error and success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const AccessService = require("../services/access.service");
+const { OK, CREATED } = require('../core/success.response');
 
 class AccessController {
     // sign up
     signUp = async(req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED({
+            message: 'Registered Successfully',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res);
     }
 }
 
